c302b3ba714f0d88c90f6d5068b6263f91eb1ab2
Ben Haas <ben@benhaas.io>
2024-03-07 11:27:22 -0800

    add basic Package class

--- reader.py
+++ reader.py
@@ -1,16 +1,13 @@
 import csv
+from package import Package
 
 with open("./data/packages.csv", mode="r") as file:
     reader = csv.reader(file)
     for row in reader:
-        print(row)
-
+        Package(row[0], row[1], row[2], row[3], row[4], row[5], row[6], row[7])
+        print(Package)
 with open("./data/addresses.csv", mode="r") as file:
     reader = csv.reader(file)
-    for row in reader:
-        print(row)
 
 with open("./data/distances.csv", mode="r") as file:
     reader = csv.reader(file)
-    for row in reader:
-        print(row)
