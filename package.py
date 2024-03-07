9fec8d1217daaddec131d2db4886279caa0e9a0e
Ben Haas <ben@benhaas.io>
2024-03-07 12:39:28 -0800

    add skeleton, fix csv, change package class

--- package.py
+++ package.py
@@ -1,10 +1,13 @@
 class Package:
-    def __init__(self, id, address, city, state, zip, deadline, weight, notes):
-        self.id = id
-        self.address = address
-        self.city = city
-        self.state = state
-        self.zip = zip
-        self.deadline = deadline
-        self.weight = weight
-        self.notes = notes
+    def __init__(self, pkg):
+        self.id = int(pkg[0])
+        self.address = pkg[1]
+        self.city = pkg[2]
+        self.state = pkg[3]
+        self.zip = pkg[4]
+        self.deadline = pkg[5]
+        self.weight = int(pkg[6])
+        self.notes = pkg[7]
+
+    def __str__(self):
+        return f"Package {self.id}, {self.address}, {self.city}, {self.state}, {self.zip}, {self.deadline}, {self.weight}, {self.notes}"
--- reader.py
+++ reader.py
@@ -1,11 +1,11 @@
 import csv
 from package import Package
 
-with open("./data/packages.csv", mode="r") as file:
+with open("./data/packages.csv", mode="r", encoding="utf-8-sig", newline="") as file:
     reader = csv.reader(file)
     for row in reader:
-        Package(row[0], row[1], row[2], row[3], row[4], row[5], row[6], row[7])
-        print(Package)
+        print(Package(row))
+
 with open("./data/addresses.csv", mode="r") as file:
     reader = csv.reader(file)
 
