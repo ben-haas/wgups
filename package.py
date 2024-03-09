41b21b6fba945676619d4e1eef873d20f77c1b5c
Ben Haas <ben@benhaas.io>
2024-03-08 16:01:20 -0800

    Python classes don't need getters : )

--- package.py
+++ package.py
@@ -42,44 +42,5 @@ class Package:
     def update_en_route_time(self, en_route_time):
         self.en_route_time = en_route_time
 
-    def get_id(self):
-        return self.id
-
-    def get_address(self):
-        return self.address
-
-    def get_city(self):
-        return self.city
-
-    def get_state(self):
-        return self.state
-
-    def get_zip(self):
-        return self.zip
-
-    def get_deadline(self):
-        return self.deadline
-
-    def get_weight(self):
-        return self.weight
-
-    def get_notes(self):
-        return self.notes
-
-    def get_status(self):
-        return self.status
-
-    def get_location(self):
-        return self.location
-
-    def get_truck_id(self):
-        return self.truck_id
-
-    def get_delivery_time(self):
-        return self.delivery_time
-
-    def get_en_route_time(self):
-        return self.en_route_time
-
     def __str__(self):
         return f"Package {self.id} is at {self.location} and scheduled for delivery to {self.address} by {self.deadline}."
